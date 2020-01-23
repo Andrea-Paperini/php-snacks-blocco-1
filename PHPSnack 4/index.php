528e7f17bf92c3f46c907359074d4e99b61100a7
Andrea-Paperini <etabeta99@live.it>
2020-01-23 11:17:40 +0100

    controllo sul 4

--- PHPSnack 4/index.php
+++ PHPSnack 4/index.php
@@ -1,7 +1,17 @@
 <?
 // ciclo 15 volte e uso la funzione rand per generare numeri casuali
-for ($i = 0; $i < 15; $i++) {
-$numero = rand(0,100);
-echo $numero;
+$numeriestratti = [];
+
+while (count($numeriestratti) < 15) {
+    // genero un numero casuale
+    $numero = rand(0,100);
+    // controllo se il numero non è già stato estratto
+    if (in_array($numero, $numeriestratti)== false) {
+        // se è vero, lo aggiungo ai numeri estratti
+        $numeriestratti [] = $numero;
+    }
+}
+foreach ($numeriestratti as $numeroestratto) {
+    echo $numeroestratto."<br>";
 }
 ?>
